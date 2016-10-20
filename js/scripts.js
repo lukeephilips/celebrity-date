edec486d943ab1cfb5634e7c2c8db52f94753f31
Jonathan Thom and Luke Philips <luke@>
2016-10-20 10:22:31 -0700

    Javascript variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+  $("form").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("input:radio[name=gender]:checked").val();
+    var sign = parseInt($("#sign").val());
+    console.log(sign + "  " + gender + "  " + age)
+    event.preventDefault();
+  });
 });
