35c6685feaf688c24a9f8f7f28072eaadb939515
Jonathan Thom and Luke Philips <luke@>
2016-10-20 10:54:29 -0700

    Add branching decision tree for celeb match

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,55 @@ $(document).ready(function() {
     var sign = parseInt($("#sign").val());
     console.log(sign + "  " + gender + "  " + age)
     event.preventDefault();
+
+    if (gender === "male") {
+      if (age > 30) {
+        if (sign <=6) {
+          $("form").hide();
+          $(".dolly").show();
+        } else {
+          $("form").hide();
+          $(".judi").show();
+        }
+      } else if (age <= 30) {
+        if (sign <=6) {
+          $("form").hide();
+          $(".jlaw").show();
+        } else {
+          $("form").hide();
+          $(".miley").show();
+        }
+      } else {
+        //console.log("got to else")
+        //$("#age").css("background-color", "red");//
+        $(".form-group#age-group").addClass("has-error");
+        console.log()
+      }
+    }
+
+    if (gender === "female") {
+      if (age > 30) {
+        if (sign <=6) {
+          $("form").hide();
+          $(".wilford").show();
+        } else {
+          $("form").hide();
+          $(".alan").show();
+        }
+      } else if (age <= 30) {
+        if (sign <=6) {
+          $("form").hide();
+          $(".chris").show();
+        } else {
+          $("form").hide();
+          $(".aziz").show();
+        }
+      } else {
+        //console.log("got to else")
+        //$("#age").css("background-color", "red");//
+        $(".form-group#age-group").addClass("has-error");
+        console.log()
+      }
+    }
   });
 });
